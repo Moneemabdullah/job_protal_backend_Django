93534895133c3ab0f02cc35a1cbdfb2ee24d3736
moneem <moneem.all.abdullah@gmail.com>
2025-02-26 22:58:21 +0600

    host

--- workwave/settings.py
+++ workwave/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-=uak7caj+7i^&ij&v35on@@tf=q)%hz2$tc5lqaygc&hfxhk57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['job-protal-api.onrender.com', '0.0.0.0', 'localhost']
 
 # Application definition
 
