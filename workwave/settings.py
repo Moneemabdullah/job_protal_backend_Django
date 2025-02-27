b51b6341d52fe3f5379fbf4b20f94ff8aa021466
moneem <moneem.all.abdullah@gmail.com>
2025-02-27 22:59:35 +0600

    csrf

--- workwave/settings.py
+++ workwave/settings.py
@@ -62,6 +62,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'https://job-protal.onrender.com',  # Frontend URL
+    'https://job-protal-api.onrender.com',
 ]
 
 CORS_ALLOW_METHODS = [
